0d0fac030ae0eb37165833f63a2660d8dc069afd
Janmejay2005 <86195670+Janmejay2005@users.noreply.github.com>
2024-10-27 02:12:48 +0530

    Update Swapping Two Numbers.c

--- Swapping Two Numbers.c
+++ Swapping Two Numbers.c
@@ -2,19 +2,18 @@
 int main()
 {
 
-int a,b;
-int res;
+int a,b,res;
 printf("Enter an number:");
 scanf("%d %d",&a,&b);
-printf("\nthe first number is=%d",a);
-printf("\nthe second number is =%d",b);
+printf("\nthe first number is = %d",a);
+printf("\nthe second number is = %d",b);
 
 res=a;
 a=b;
 b=res;
 
 printf("\nafter swapping");
-printf("\nthe first number is=%d",a);
-printf("\nthe second number is=%d",b);
+printf("\nthe first number is = %d",a);
+printf("\nthe second number is = %d",b);
 return 0;
 }
